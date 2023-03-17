86182509e132d7f04089a3a6bbc22da98044ca63
iRyann <ryanbouchou@orange.fr>
2023-03-17 14:14:32 +0100

    modified:   "traitement_des_donn\303\251es2.py"

--- filelab.py
+++ filelab.py
@@ -1,34 +1,79 @@
+import sqlite3 as sql
+import matplotlib.pyplot as plt
 import math as m 
+import numpy as np
+#---------------------------------------------------
+#           Fonctions auxiliaires
+#---------------------------------------------------
 
-lat=m.radians(43.54946816425778)
-lat_t=m.radians(43.54946816425778)
-long=m.radians(1.37651356077766)
-long_t=m.radians(1.506607510636402)
-x=m.cos(lat)*m.cos(lat_t)*((m.sin((long-long_t)/2))**2)
-r=6371*(10**3)
-y=m.asin(m.sqrt(x))
-d=2*r*y
-
-print(y)
-print("res:",d)
-
-
-p=2*r*m.asin(m.sqrt((m.sin((lat-lat_t)/2)**2)+m.cos(lat)*m.cos(lat_t)*(m.sin((long-long_t)/2))**2))
-print(p)
-a=[long_t,lat_t]
-b=[long,lat]
+def est_valide (tab):
+    #Retourne si dist(tab,capitole)<10 Km où tab=[long,lat]
+    lat=m.radians(tab[1])
+    long=m.radians(tab[0])
+    r=6371*(10**3)
+    lat_t=m.radians(43.60395967066511)
+    long_t=m.radians(1.4433469299842416)
+    d=2*r*m.asin(m.sqrt((m.sin((lat-lat_t)/2)**2)+m.cos(lat)*m.cos(lat_t)*(m.sin((long-long_t)/2))**2))
+    return (d<500)
 
 def distance (a,b):
     #Calcule d(a,b) en m
     lat=m.radians(a[1])
-    long=m.radians(b[0])
+    long=m.radians(a[0])
     r=6371*(10**3)
     lat_t=m.radians(b[1])
     long_t=m.radians(b[0])
-    p=2*r*m.asin(m.sqrt((m.sin((lat-lat_t)/2)**2)+m.cos(lat)*m.cos(lat_t)*(m.sin((long-long_t)/2))**2))
-    return p
-
-f=distance(a,b)
-print("rresdisatn:",f)
+    d=2*r*m.asin(m.sqrt((m.sin((lat-lat_t)/2)**2)+m.cos(lat)*m.cos(lat_t)*(m.sin((long-long_t)/2))**2))
+    return d
 
 
+
+def affiche_graphe( d : dict, tab : list):
+    fig, ax = plt.subplots()
+    #----- Affichage de la grille ----------------------
+    grid_x_ticks = np.arange(gd["latmin"],gd["latmax"], 0.000001)
+    grid_y_ticks = np.arange(gd["longmin"], gd["longmin"], 0.000001)
+
+    ax.set_xticks(grid_x_ticks , minor=True)
+    ax.set_yticks(grid_y_ticks , minor=True)
+    print("coucou")
+    ax.grid(which='both')
+
+    ax.grid(which='minor', alpha=0.2, linestyle='--')
+    #----------------------------------------------------
+    z=0
+    a=43.60395967066511
+    b=1.4433469299842416
+    ax.scatter(b,a,s=5)
+    for k,v in d.items():
+        for j in v.keys():
+
+            x=[tab[j][0],tab[k][0]]
+            y=[tab[j][1],tab[k][1]]
+            e=tab[j][0]-tab[k][0]
+            if e==0:
+                e=10**(-16)
+
+
+            xx = np.linspace(tab[j][0],tab[k][0], 100)
+            yy = ((tab[j][1]-tab[k][1])/(e))*(xx-tab[k][0])+tab[k][1]
+
+            ax.plot(xx, yy, linewidth=0.5)
+
+            ax.scatter(x, y, s=2.5)
+            z+=1
+    plt.plot(abs,ord,'g^')        
+    plt.show()
+
+
+
+
+#4/ Traitement du graphe 
+
+## --------- Ajout de la population ------------ #
+
+
+
+q=[] # Partitionnement des noeuds par quartier
+
+partition_vide(q,,1)
--- traitement_des_données2.py
+++ traitement_des_données2.py
@@ -14,7 +14,7 @@ def est_valide (tab):
     lat_t=m.radians(43.60395967066511)
     long_t=m.radians(1.4433469299842416)
     d=2*r*m.asin(m.sqrt((m.sin((lat-lat_t)/2)**2)+m.cos(lat)*m.cos(lat_t)*(m.sin((long-long_t)/2))**2))
-    return (d<500)
+    return (d<1000)
 
 def distance (a,b):
     #Calcule d(a,b) en m
@@ -27,6 +27,37 @@ def distance (a,b):
     return d
 
 #---------------------------------------------------
+#   Fonction d'affichage
+#---------------------------------------------------
+
+def affiche_graphe( d : dict, tab : list):
+    fig, ax = plt.subplots()
+    #----- Affichage de la grille ----------------------
+    
+    #----------------------------------------------------
+    z=0
+    a=43.60395967066511
+    b=1.4433469299842416
+    ax.scatter(b,a,s=5)
+    for k,v in d.items():
+        for j in v.keys():
+
+            x=[tab[j][0],tab[k][0]]
+            y=[tab[j][1],tab[k][1]]
+            e=tab[j][0]-tab[k][0]
+            if e==0:
+                e=10**(-16)
+
+
+            xx = np.linspace(tab[j][0],tab[k][0], 100)
+            yy = ((tab[j][1]-tab[k][1])/(e))*(xx-tab[k][0])+tab[k][1]
+
+            ax.plot(xx, yy, linewidth=0.5)
+
+            print(z)
+            z+=1
+    plt.plot(abs,ord,'g^')        
+    plt.show()
 
 
 
@@ -109,10 +140,12 @@ t=recupere_data(f,con,requete2)
 #           Traitement des données
 #---------------------------------------------------
 
-g={} #Graphe
-tab=[] #Coordonnées associées à un pt une graphe
-noeud_valide=[]
+
 # 1/ Création du graphe
+
+g={} #Graphe dict de dict 
+tab=[] #Coordonnées associées à un pt une graphe
+noeud_valide=[] # Un noeud est dit valide (ie à "true") ssi il est à considérer après fusion du graphe 
 
 def ajoute_voisin(g : dict, i : int , c : tuple ) :
     #Ajoute la relation i->j et j->i telle que d(i,j)=dist
@@ -124,15 +157,19 @@ def ajoute_voisin(g : dict, i : int , c : tuple ) :
         print(i,j)
         raise ValueError("Relation déjà présente")
 
+abs=[]
+ord=[]
 
 def ajoute_noeud(g : dict, t : list, tab : list):
     n=0 #nombre de noeuds
     
-    for i in range(0,len(t)-1):
+    for i in range(0,len(t)):
         
         for j in range(0,len(t[i])):
             g[n]={}
             tab.append(t[i][j])
+            abs.append(t[i][j][0])
+            ord.append(t[i][j][1])
             noeud_valide.append(True)
             n+=1
         #Complétion du voisinnage
@@ -140,18 +177,22 @@ def ajoute_noeud(g : dict, t : list, tab : list):
             for j in range (0,len(t[i])-1):
                 # On ajoute les relations ( j <-> j+1 ) entre les sommets intermédiaire d'une même route
                 ajoute_voisin(g,n-len(t[i])+j,(n-len(t[i])+j+1,distance(t[i][j],t[i][j+1])))
+    
 
 #On complete le graphe g grâce aux données récupérées dans t
 #en retenant les coordonnées de chaque noeud de g dans tab
 ajoute_noeud(g,t,tab)
 
+
+# 2/ Complétion de la partnoeuds 
+
 ##Calcul distance partition
-nlat=int(distance([gd["longmin"],gd["latmin"]],[gd["longmin"],gd["latmax"]])/2)
-nlong=int(distance([gd["longmin"],gd["latmin"]],[gd["longmax"],gd["latmin"]])/2)
+eta = 3 #distance de partionnement
+nlat=int(distance([gd["longmin"],gd["latmin"]],[gd["longmin"],gd["latmax"]])/eta)
+nlong=int(distance([gd["longmin"],gd["latmin"]],[gd["longmax"],gd["latmin"]])/eta)
 
-# 3/ Complétion de la partnoeuds 
-partition_vide(partnoeuds,nlat,nlong)
 
+partition_vide(partnoeuds,nlat,nlong) # On crée la partition vide 
 
 tabclasse=[] # Pour chaque noeud, on retient sa classe. 
 
@@ -164,28 +205,31 @@ def add_part(gg : dict, t: list):
         
         N=0
         E=0
-        dlat=distance([gd["longmin"],tab[k][1]],tab[k])
-        dlong=distance([tab[k][0],gd["latmin"]],tab[k])
+        dlong=distance([gd["longmin"],tab[k][1]],tab[k])
+        dlat=distance([tab[k][0],gd["latmin"]],tab[k])
         
         
         while(not(N*nlong<=dlong and dlong<=(N+1)*nlong)):
             N+=1
+            
            
             
         while(not(E*nlat<=dlat and dlat<=(E+1)*nlat)):
             E+=1
             
-        partnoeuds[N][E].append(k)
-        print("On ajoute",k)
-        tabclasse.append([N,E])
+        partnoeuds[N][E].append(k) #On ajoute le noeud dans la partition
+        
+        tabclasse.append([N,E]) #On retient la classe du noeud 
     print("Création de la partition terminée")
 
 add_part(g,tab)
 
 
-# 2/ Correction du graphe
+
+
+# 3/ Correction du graphe
   
-    
+    # calculer le dregre max
 def fusion ( i : int , j : int ):
     # Fusionne le noeud i et j, en attibuant remplaçant j par i dans les voisins de celui-ci
     for k,v in g[j].items():
@@ -201,30 +245,33 @@ def apptab(x : int, y : int ):
     return(x>=0 and y>=0 and x<nlong and y<nlat)
 
 
-def classement(g:dict, t : list ):
-    ##Classe le dictionnaire g en recquérant les fusions nécessaires
+def classement(g:dict, t : list , eps : float):
+    ## Classe le dictionnaire g en recquérant les fusions proches à eps près
+    ## Précondition : eps<eta
     for k in g.keys():
-        if bool(k):
+        if noeud_valide[k]: #Teste si k n'a pas déjà été fusionné 
             X=k
-            temp=[]
-            n=tabclasse[k][0]
-            e=tabclasse[k][1]
+            a_fusionner=[]
+            # a_fusionner U {X} est l'ensemble des noeuds à distance < esp déjà trouvés 
+            # X est celui d'identifiant minimal
+            n,e=tabclasse[k]
 
             for i in range(-1,2):
                 for j in range(-1,2):
                     if apptab(n+i,e+j):
                         for z in partnoeuds[n+i][e+j]:
-                            if distance(t[z],t[k])<2 and z!=X:
+                            if noeud_valide[z] and distance(t[z],t[k])<eps and z!=X:
                                 if z<X:
+                                    a_fusionner.append(X)
                                     X=z
                                 else: 
-                                    temp.append(z)
-            for i in temp:
+                                    a_fusionner.append(z)
+            for i in a_fusionner:
                 fusion(X,i)
-                print("Noeud",i,"fusionné avec le noeud",X)
-                g[i]={}
+                assert(not(noeud_valide[i]))
 
-classement(g,tab)
+classement(g,tab,0.9*eta) # Attention eps < eta
+#affiche_graphe(g,tab)
 g_final={}
 gf_coord=[]
 
@@ -241,44 +288,17 @@ def renum( t1 : list , t2 : list, g1 : dict, gf : dict):
     
     for k,v in g1.items():
         if table[k]!=-1:
+            o=int(table[k])
+            gf[o]={}
             for kk,vv in v.items():
                 if table[kk]!=-1:
-                    o=int(table[k])
                     p=int(table[kk])
-                    gf[o]={}
                     gf[o][p]=vv
 
 
 renum(noeud_valide,gf_coord,g,g_final)
 
-
-
-
-
-def affiche_graphe( d : dict, tab : list):
-    fig, ax = plt.subplots()
-    z=0
-    a=43.60395967066511
-    b=1.4433469299842416
-    ax.scatter(b,a,s=5)
-    for k,v in d.items():
-        for j in v.keys():
-
-            x=[tab[j][0],tab[k][0]]
-            y=[tab[j][1],tab[k][1]]
-            e=tab[j][0]-tab[k][0]
-            if e==0:
-                e=10**(-16)
-
-
-            xx = np.linspace(tab[j][0],tab[k][0], 100)
-            yy = ((tab[j][1]-tab[k][1])/(e))*(xx-tab[k][0])+tab[k][1]
-
-            ax.plot(xx, yy, linewidth=0.5)
-
-            ax.scatter(x, y, s=2.5)
-            z+=1
-            print(k)
-    plt.show()
+print("Taille avant fusion:",len(g),"Taille après fusion",len(g_final))
 
 affiche_graphe(g_final,gf_coord)
+
